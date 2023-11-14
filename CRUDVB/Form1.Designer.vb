145159f492cbfbfebbbdf7a5dbec7b9277907274
Hadinmd <hadinurmuhamad8@gmail.com>
2023-11-14 17:07:19 +0700

    init

---
+++ CRUDVB/Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        DataGridView1 = New DataGridView()
+        ComboBox1 = New ComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        DateTimePicker1 = New DateTimePicker()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(146, 311)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(96, 40)
+        Button1.TabIndex = 0
+        Button1.Text = "Input "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(146, 82)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(210, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(146, 126)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(210, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(146, 227)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(210, 23)
+        TextBox3.TabIndex = 6
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(260, 311)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(96, 40)
+        Button2.TabIndex = 8
+        Button2.Text = "Edit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(146, 367)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(96, 40)
+        Button3.TabIndex = 9
+        Button3.Text = "Delete"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(260, 367)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(96, 40)
+        Button4.TabIndex = 10
+        Button4.Text = "Close"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(444, 82)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(590, 209)
+        DataGridView1.TabIndex = 11
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Obat Ringan", "Obat Keras"})
+        ComboBox1.Location = New Point(146, 178)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(210, 23)
+        ComboBox1.TabIndex = 12
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(56, 86)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(66, 15)
+        Label1.TabIndex = 13
+        Label1.Text = "Kode Obat"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(56, 127)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(69, 15)
+        Label2.TabIndex = 14
+        Label2.Text = "Nama Obat"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(56, 179)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 15)
+        Label3.TabIndex = 15
+        Label3.Text = "Jenis Obat"
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(146, 270)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(210, 23)
+        DateTimePicker1.TabIndex = 18
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(56, 235)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(36, 15)
+        Label4.TabIndex = 19
+        Label4.Text = "Dosis"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(56, 276)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(79, 15)
+        Label5.TabIndex = 20
+        Label5.Text = "Expired Date"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(56, 28)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(227, 32)
+        Label6.TabIndex = 21
+        Label6.Text = "Jaya Abadi Apotek"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(444, 478)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(238, 15)
+        Label7.TabIndex = 22
+        Label7.Text = "Hadi Nur Muhammad  | NPM 1101221140"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(817, 47)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(204, 32)
+        Label8.TabIndex = 23
+        Label8.Text = "Form Input Obat"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(1108, 502)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Apotek Jaya Abadi"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ CRUDVB/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Data.Odbc
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim Conn As OdbcConnection
+    Dim cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
+
+    Sub Connection()
+        MyDB = "Driver={MySQL ODBC 8.1 ANSI Driver};Database=apotekerdb;server=localhost;uid=root"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+    End Sub
+
+    Sub KondisiAwal()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+        ComboBox1.Text = ""
+
+        Call Connection()
+        Da = New OdbcDataAdapter("select * from apoteker", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "apoteker")
+        DataGridView1.DataSource = Ds.Tables("apoteker")
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call KondisiAwal()
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call Connection()
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss") = "" Or ComboBox1.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Dim InputData As String = " Insert into apoteker value ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & ComboBox1.Text & "','" & TextBox3.Text & "','" & DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss") & "')"
+            cmd = New OdbcCommand(InputData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Input Data Berhasil")
+            Call KondisiAwal()
+        End If
+    End Sub
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Call Connection()
+            cmd = New OdbcCommand("select * from apoteker where KodeObat= '" & TextBox1.Text & "'", Conn)
+
+            Rd = cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                TextBox2.Text = Rd.Item("NamaObat")
+                ComboBox1.Text = Rd.Item("JenisObat")
+                TextBox3.Text = Rd.Item("Dosis")
+                DateTimePicker1.Value = Rd.Item("ExpireDate")
+            Else
+                MsgBox("Data tidak ditemukan")
+            End If
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Call Connection()
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss") = "" Or ComboBox1.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Dim DeleteData As String = "delete from apoteker where KodeObat='" & TextBox1.Text & "'"
+            cmd = New OdbcCommand(DeleteData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Hapus Data Berhasil")
+            Call KondisiAwal()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss") = "" Or ComboBox1.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Call Connection()
+            Dim EditData As String = "update apoteker Set NamaObat='" & TextBox2.Text & "',JenisObat='" & ComboBox1.Text & "',Dosis='" & TextBox3.Text & "',ExpireDate='" & DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss") & "' where KodeObat='" & TextBox1.Text & "'"
+            cmd = New OdbcCommand(EditData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Edit Data Berhasil")
+            Call KondisiAwal()
+        End If
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+End Class
